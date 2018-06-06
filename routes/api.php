4048f7649d5cd4fa527ea1422429bdc9aa2a16d1
Monopeelz <monopeelz@gmail.com>
2018-06-06 15:05:16 +0700

    add test route

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,18 @@ $route->group(getconst('api_prefix'), function() {
     $this->get('/test', function () {
         echo json(this());
     });
+
+    $this->get('articles', function(){
+        echo json_encode([
+            [
+                'id' => 1,
+                'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+                'header' => 'Header1',
+                'creator' => [
+                    'name' => 'First Last'
+                ],
+                'date' => '20/05/2018 sss'
+            ]
+        ]);
+    });
 });
