f69d5a587ee0ba09ff6a403fa2b85088b7a2c347
Apinan Woratrakun <apinanworatrakun@Apinans-MacBook-Pro.local>
2018-06-21 13:08:49 +0700

    update add classroom

--- app/controllers/GradeControllers.php
+++ app/controllers/GradeControllers.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 use \App\dbClient;
 use \App\MiddleWare\Authenticate;
 use \App\MiddleWare\BasicAuthen;
+
 class GradeControllers{
     private $obj = [];
 
@@ -20,5 +21,35 @@ class GradeControllers{
             "data" => $data
         ];
         echo json($res);
+    }
+
+    public function postClassroom() {
+        if(!empty(req('grade')) && !empty(req('email')) && !empty(req('classNumber'))) {
+            $user = $this->db->selectOne('users', ['id(user_id)', 'school_id'], ['username' => req('email')]);
+
+            $data = [
+                "user_id" => $user['user_id'],
+                "grade" => req('grade'),
+                "class" => req('classNumber'),
+                "school_id" => $user['school_id']
+            ];
+
+            $this->db->insert('classrooms', $data);
+
+            if($this->db->exec()->id() != 0) {
+                $res = [
+                    "status" => "success",
+                    "data" => $data
+                ];
+                echo json($res, 200);
+            } else {
+                $res = [
+                    "status" => "error",
+                    "msg" => "ข้อมูลบางอย่างไม่ถูกต้อง"
+                ];
+                echo json($res, 501);
+            }
+
+        } 
     }
 }
