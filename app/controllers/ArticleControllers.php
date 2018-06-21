b68b571794f420e1315436390237ad37f9e80983
Apinan Woratrakun <apinanworatrakun@Apinans-MacBook-Pro.local>
2018-06-21 18:23:29 +0700

    update article add

--- app/controllers/ArticleControllers.php
+++ app/controllers/ArticleControllers.php
@@ -26,4 +26,25 @@ class ArticleControllers{
         $res['timestamp'] = ($res['timestamp'] != null) ? date('d/m/Y H:i', $res['timestamp']) : time();
         echo json($res);
     }
+
+    public function postAdd() {
+        $school = $this->db->selectOne("users", ['school_id'], ["id" => req('userid')]);
+        $data = [
+            "title" => req('title'),
+            "description" => req("description"),
+            "uid" => req('userid'),
+            "school_id" => $school['school_id'],
+            "image" => "https://picsum.photos/200/300",
+            "timestamp" => time()
+        ];
+        $this->db->insert("article_data", $data);
+
+        if($this->db->exec()->id() != 0) {
+            $res = [
+                "status" => "success",
+                "data" => $data
+            ];
+            echo json($res);
+        }
+    }
 }
