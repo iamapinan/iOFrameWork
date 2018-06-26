23bd244617f33dd7316455c7317c3195cdc0e2b5
Apinan Woratrakun <apinanworatrakun@Apinans-MacBook-Pro.local>
2018-06-26 18:12:25 +0700

    update add chat api

--- app/controllers/ChatControllers.php
+++ app/controllers/ChatControllers.php
@@ -15,14 +15,59 @@ class ChatControllers{
         $basic->Check();
     }
 
-    public function getGroup($type, $userid) {
+    public function postChatroom() {
+
+        if(!empty(req('user_id'))) {
+            $user = $this->db->selectOne('users', ['id', 'school_id', 'role_id'], ['id' => req('user_id')]);
+        
+            if($user['role_id'] == 1) {
+
+                $data = [
+                    "title" => req('name'),
+                    "user_id" => $user['id'],
+                    "code" => strtoupper(rand(1, 9) . substr(uniqid(), 0, 4) . chr(rand(ord('a'), ord('z')))),
+                    "school_id" => $user['school_id']
+                ];
+    
+                $this->db->insert('chat_room', $data);
+            } elseif(!empty(this()->body['code'])) {
+                // require chatroom code
+                $chatroom = $this->db->select('chat_room', ['id'], ['code' => req('code')]);
+
+                if(count($chatroom)!=0) {
+                    $data = [
+                        "user_id" => req('user_id'),
+                        "chat_room_id" => $chatroom[0]['id']
+                    ];
+                    $this->db->insert('chat_room_member', $data);
+                }
+            }
+
+            if($this->db->exec()->id() != 0) {
+                $res = [
+                    "status" => "success",
+                    "data" => $data
+                ];
+                echo json($res, 200);
+            } else {
+                $res = [
+                    "status" => "error",
+                    "msg" => "ข้อมูลบางอย่างไม่ถูกต้อง"
+                ];
+                echo json($res, 501);
+            }
+
+        } 
+    }
+
+    public function getGroup($userid) {
         if($userid != '') {
-            $list = $this->db->select("classrooms", "*", ["user_id" => $userid]); //get room info
+            $list = $this->db->select("chat_room", "*", ["user_id" => $userid]); //get room info
             for($x=0;$x<count($list);$x++) {
                 $chat = $this->db->exec()->select("chat_msg", 
                 ["[>]users" => ["user_id" => "id"]],
                 ["chat_msg.msg", "chat_msg.timestamp", "chat_msg.user_id", "users.first_name(user_name)"],
-                ["chat_msg.target_id" => $list[$x]['id'], "chat_msg.type" => $type, "LIMIT" => 1, "ORDER" => ["timestamp" => "DESC"]]
+                ["chat_msg.target_id" => $list[$x]['id'], "LIMIT" => 1, "ORDER" => ["timestamp" => "DESC"]]
                 );
                 // echo json($chat);
                 if(count($chat) > 0) {
@@ -42,21 +87,19 @@ class ChatControllers{
     }
 
     public function getChatroom($roomid) {
-        $chat = $this->db->exec()->select("classrooms", 
+        $chat = $this->db->exec()->select("chat_room", 
         [
-        '[>]schools' => ["school_id" => "id"],
-        '[>]educations' => ["grade" => "id"]
+        '[>]schools' => ["school_id" => "id"]
         ],
         [
-            "schools.name(school_name)", "classrooms.id(chat_id)", "classrooms.title",
-            "classrooms.user_id","classrooms.grade", "educations.name(grade_name)"
+            "schools.name(school_name)", "chat_room.id(chat_id)", "chat_room.title","chat_room.user_id"
         ], // Fields
-        ["classrooms.id" => $roomid]); // Where condition
+        ["chat_room.id" => $roomid]); // Where condition
 
-        $member = $this->db->select("student_classrooms", 
+        $member = $this->db->select("chat_room_member", 
         [ "[>]users" => ["user_id" => "id"], "[>]roles" => ["id" => "id"] ], 
         ["users.first_name", "users.last_name", "users.id", "users.photo", "roles.name(role_name)"],
-        ["student_classrooms.classroom_id" => $roomid]);
+        ["chat_room.id" => $roomid]);
 
         $msg = $this->db->PaginationMulti("chat_msg", 
             ["[>]users" => ["user_id" => "id"]],
--- app/controllers/GradeControllers.php
+++ app/controllers/GradeControllers.php
@@ -24,51 +24,8 @@ class GradeControllers{
         echo json($res);
     }
 
-    public function getClass($school, $grade) {
-        $class = $this->db->select("classrooms", ["id", "title"], ["school_id"=>$school, "grade" => $grade]);
+    public function getClass($school, $userid) {
+        $class = $this->db->select("chat_room", ["id", "title"], ["school_id"=>$school, "user_id" => $userid]);
         echo json($class, 200);
-    }
-
-    public function postClassroom() {
-
-        if(!empty(req('user_id'))) {
-            $user = $this->db->selectOne('users', ['id', 'school_id', 'role_id'], ['id' => req('user_id')]);
-        
-            if($user['role_id'] == 1) {
-
-                $data = [
-                    "title" => req('name'),
-                    "user_id" => $user['id'],
-                    "code" => strtoupper(rand(1, 9) . substr(uniqid(), 0, 4) . chr(rand(ord('a'), ord('z')))),
-                    "school_id" => $user['school_id']
-                ];
-    
-                $this->db->insert('chat_room', $data);
-            } else {
-                $chatroom = $this->db->select('chat_room', ['id'], ['code' => req('code')]);
-                // require chatroom code
-                $data = [
-                    "user_id" => req('user_id'),
-                    "chat_room_id" => $chatroom[0]['id']
-                ];
-    
-                $this->db->insert('chat_room_member', $data);
-            }
-
-            if($this->db->exec()->id() != 0) {
-                $res = [
-                    "status" => "success",
-                    "data" => $data
-                ];
-                echo json($res, 200);
-            } else {
-                $res = [
-                    "status" => "error",
-                    "msg" => "ข้อมูลบางอย่างไม่ถูกต้อง"
-                ];
-                echo json($res, 501);
-            }
-
-        } 
     }
 }
